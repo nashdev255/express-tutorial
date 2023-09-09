acde74bda942b8ea7dd52bfde6af3ac8a85c41bc
nashdev255 <yutori1107@gmail.com>
2023-09-09 12:51:06 +0900

    ejs yokunai

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const itemRouter = require('./routes/item');
 const PORT = 3000;
 
 // use html files in /public/
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -14,7 +14,8 @@ app.get('/', (req, res) => {
     // res.send('<h1>Hello, World</h1>');
     // res.sendStatus(404);
     // res.status(404).send('お探しのページは見つかりませんでした');
-    res.status(200).json({ msg: 'エラーです' });
+    // res.status(200).json({ msg: 'エラーです' });
+    res.render('index', { text: 'nodeとexpress' });
 });
 
 // Routing
